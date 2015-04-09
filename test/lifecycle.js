12b521974997624fe656391d338263713457bae6
David Pate <davidtpate@gmail.com>
2015-04-09 15:36:37 -0400

    Swap the parameters in the two files that I missed.

--- test/lifecycle.js
+++ test/lifecycle.js
@@ -44,7 +44,7 @@
                 region: process.env.AWS_REGION
             };
             bucketName = 's3fs-lifecycle-test-bucket-' + (Math.random() + '').slice(2, 8);
-            s3fsImpl = new S3FS(s3Credentials, bucketName);
+            s3fsImpl = new S3FS(bucketName, s3Credentials);
 
             return s3fsImpl.create();
         });
